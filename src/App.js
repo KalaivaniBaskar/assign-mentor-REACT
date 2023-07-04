8829cd0619da067550380e83ffc0af816e4a3e1c
KalaivaniBaskar <kalaivani.working@gmail.com>
2023-07-04 08:37:08 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Header from './Components/Header';
+import Home from './Components/Home';
+import Student from './Components/Student';
+import Students from './Components/Students';
+import Mentor from './Components/Mentor';
+import Mentors from './Components/Mentors';
+import AssignMentor from './Components/AssignMentor';
+import AssignStudents from './Components/AssignStudents';
+import AddStudent from './Components/AddStudent';
+import AddMentor from './Components/AddMentor';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Header />
+      
+      <Routes>
+        <Route path="/" element={ <Home /> } ></Route>
+        <Route path="/students" element={ <Students /> } ></Route>
+        <Route path="/add-student" element={ <AddStudent /> } ></Route>
+        <Route path="/mentors" element={ <Mentors /> } ></Route>
+        <Route path="/add-mentor" element={ <AddMentor /> } ></Route>
+        <Route path="/assign-mentor/:id" element={ <AssignMentor /> } ></Route>
+        <Route path="/assign-students/:id" element={ <AssignStudents /> } ></Route>
+        <Route path="/student/:id" element={ <Student /> } ></Route>
+        <Route path="/mentor/:id" element={ <Mentor /> } ></Route>
+      </Routes>
     </div>
   );
 }
